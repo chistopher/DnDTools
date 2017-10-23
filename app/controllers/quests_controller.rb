341516b100ee8cd83859effdd4110a71e6349a91
christopher <christopher.weyand@student.hpi.de>
2017-10-23 13:18:09 +0200

    Quests

--- app/controllers/quests_controller.rb
+++ app/controllers/quests_controller.rb
@@ -1,4 +1,25 @@
 class QuestsController < ApplicationController
+
+  def index
+    @quests = Quest.all
+  end
+
   def new
   end
+
+  def create
+    @quest = Quest.new(quest_params)
+
+    @quest.save
+    redirect_to @quest
+  end
+
+  def show
+    @quest = Quest.find(params[:id])
+  end
+
+  private
+    def quest_params
+      params.require(:quest).permit(:title, :description)
+    end
 end

b627a479b656f9eb1d535d8901835d522fcb3eb1
christopher <christopher.weyand@student.hpi.de>
2017-10-26 17:41:57 +0200

    introduce admin status

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+    def authenticate
+      authenticate_or_request_with_http_basic do |user_name, password|
+        session[:user] = 'admin' if user_name == 'admin' && password == 'password'
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  get 'welcome/login'
 
   resources :quests
-
   resources :npcs
 
   root 'welcome#index'

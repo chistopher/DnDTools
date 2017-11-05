de2dafd0482a872b7a53bc13833a8775f5a03cda
christopher <christopher.weyand@student.hpi.de>
2017-11-05 13:06:49 +0100

    add spells

---
+++ db/migrate/20171105120257_create_spells.rb
@@ -0,0 +1,20 @@
+class CreateSpells < ActiveRecord::Migration[5.1]
+  def change
+    create_table :spells do |t|
+      t.string :name
+      t.text :desc
+      t.string :page
+      t.string :range
+      t.string :components
+      t.boolean :ritual
+      t.string :duration
+      t.boolean :concentration
+      t.string :casting_time
+      t.string :level
+      t.string :school
+      t.string :class
+
+      t.timestamps
+    end
+  end
+end
